cbcbe435233366b6f6f2577b138a2a98dbb3e93b
Dominic Frei <dominic.frei@mongodb.com>
2020-12-21 10:46:16 +0000

    Add empty Xcode project (including .gitignore).

---
+++ RealmBootcamp/RealmBootcamp/RealmBootcamp.h
@@ -0,0 +1,18 @@
+//
+//  RealmBootcamp.h
+//  RealmBootcamp
+//
+//  Created by Dominic Frei on 21/12/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RealmBootcamp.
+FOUNDATION_EXPORT double RealmBootcampVersionNumber;
+
+//! Project version string for RealmBootcamp.
+FOUNDATION_EXPORT const unsigned char RealmBootcampVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmBootcamp/PublicHeader.h>
+
+
